b11f525d60dd25e1ab6711ab9b7fef4db8dc47f2
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-04-07 23:02:50 +0200

    Update barcode.js

--- public/barcode/barcode.js
+++ public/barcode/barcode.js
@@ -67,10 +67,10 @@ function success(text, result) {
                 //Takes the previously "return response.json();" as input
                 if(json.msg === "adding found")
                 {
-                    alert("found " + json.found.length);
-                    // for (let i = 0; i < json.array.length; i++) {
 
-                    // }
+                    for (let i = 0; i < json.found.length; i++) {
+                        alert(found[i].name);
+                    }
                     
                 } else{
                   alert("found no item matching")
